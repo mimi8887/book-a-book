64fea0a3d2b1051544bdea8994520eb7222aa4fd
Fernanda Ngungui <franciabreu5080@gmail.com>
2024-11-19 17:56:21 +0100

    books controllerindex

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @users = User.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
