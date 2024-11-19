047f2359f4a03efbc298c3859cb046f5232aa3d4
Simone Mine <simonminekoza@gmail.com>
2024-11-19 17:55:09 +0100

    show and create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,21 @@ class BooksController < ApplicationController
 
   def new
     @book = Book.new
-    
   end
 
   def create
+    @book = Book.new(list_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:book).permit(:title, :author, :description, :condition)
   end
 end
+"#{book.first_name} #{book.last_name}"
